509bac6f12bfea2e0d2f7126809c80a5bb005f5e
rahulg009 <63463686+rahulg009@users.noreply.github.com>
2021-04-04 00:02:57 +0530

    added sequelize

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const db = require('./util/database');
 
-const app = express();
+const app = express(); 
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
