4636c848bece703b5ec299181859f505a51d9a9c
rgdevelopings <rahulgowlapalli@outlook.com>
2021-05-06 16:59:38 +0530

    association inclu

--- app.js
+++ app.js
@@ -15,16 +15,44 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const sequelize = require('./util/database');
 
+const Product=require('./models/product');
+const User=require('./models/user');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user;
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User,{
+    constraints:true,onDelete:'CASCADE'
+})
+User.hasMany(Product)
 
-sequelize.sync()
+sequelize
+// .sync({force:true})
+.sync()
+.then(result=>{
+    return User.findByPk(1);
+})
+.then(user=>{
+    if(!user){
+        return User.create({name:"Rahul",email:"rg@gmail.com"})
+    }
+    return user;
+})
 .then(result=>{
     app.listen(3000,()=>{
         console.log('connected');
