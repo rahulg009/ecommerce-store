4521e1eaa4157501508e3a4f65720b473cd1754c
rgdevelopings <rahulgowlapalli@outlook.com>
2021-05-07 13:07:51 +0530

    order model

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
-const Cart=require('./models/cart');
-const CartItem=require('./models/cart-item');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -39,11 +41,14 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product,{through:CartItem});
-Product.belongsToMany(Cart,{through:CartItem});
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-//   .sync({ force: true })
+  // .sync({ force: true })
   .sync()
   .then(result => {
     return User.findByPk(1);
@@ -51,16 +56,17 @@ sequelize
   })
   .then(user => {
     if (!user) {
-      return User.create({ name: 'rahul', email: 'rg@gmail.com' });
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
     return user;
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
     console.log(err);
   });
-
-
