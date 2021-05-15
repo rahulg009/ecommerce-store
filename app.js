cd52d968ad722dbd7f3376f1da3a30d08d392d40
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-05-15 21:25:48 +0530

    session added

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -40,8 +42,8 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Rahul',
+          email: 'rg@gmail.com',
           cart: {
             items: []
           }
@@ -50,6 +52,7 @@ mongoose
       }
     });
     app.listen(3000);
+    console.log("connected")
   })
   .catch(err => {
     console.log(err);
